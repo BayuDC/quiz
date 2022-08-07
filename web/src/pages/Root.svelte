62ad7a7a1f5aecfa6588d3b05a603de81c7dc4ba
BayuDC <dev.bayudc@gmail.com>
2022-08-07 18:23:22 +0700

    feat: radio button component

--- web/src/pages/Root.svelte
+++ web/src/pages/Root.svelte
@@ -1,15 +1,10 @@
 <script>
     import LayoutMain from '../layouts/Main.svelte';
+
+    import Radio from '../shared/Radio.svelte';
 </script>
 
 <LayoutMain>
-    <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam ratione et fuga similique soluta facilis tempora
-        hic dicta, reprehenderit non!
-    </p>
-    <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eligendi fuga, a, aspernatur esse laborum vel
-        possimus perferendis nam suscipit nemo necessitatibus, iste ipsum voluptates sequi repellendus. Porro, voluptate
-        enim aliquam deleniti inventore excepturi, esse odit quo explicabo, quasi eveniet!
-    </p>
+    <Radio name="test" id="A" value="1">Lorem ipsum</Radio>
+    <Radio name="test" id="B" value="2">Lorem, ipsum dolor.</Radio>
 </LayoutMain>
---
+++ web/src/shared/Radio.svelte
@@ -0,0 +1,50 @@
+<script>
+    export let name;
+    export let id;
+    export let value;
+</script>
+
+<div class="radio-field">
+    <input type="radio" {name} id={'radion-' + id} {value} />
+    <label for={'radion-' + id} data-content={id}>
+        <slot />
+    </label>
+</div>
+
+<style lang="scss">
+    @import '../styles/variable';
+
+    .radio-field {
+        input {
+            appearance: none;
+
+            &:checked + label::before {
+                background: $black;
+                color: $white;
+                font-weight: 700;
+            }
+        }
+
+        label {
+            display: flex;
+            align-items: center;
+            gap: 15px;
+
+            &::before {
+                content: attr(data-content);
+                padding: 8px;
+                border: 2px solid $black;
+
+                font-weight: 400;
+                width: 20px;
+                text-align: center;
+                flex-shrink: 0;
+                align-self: start;
+
+                transition: 0.3s;
+            }
+
+            margin-bottom: 20px;
+        }
+    }
+</style>
