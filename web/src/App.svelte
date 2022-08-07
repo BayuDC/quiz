d063885f6fa1e4cfe4381eb85a65f043a03875b8
BayuDC <dev.bayudc@gmail.com>
2022-08-07 19:20:08 +0700

    chore: add routing system

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { Router, Route } from 'svelte-navigator';
     import Container from './components/Container.svelte';
     import Login from './pages/Login.svelte';
     import Root from './pages/Root.svelte';
@@ -6,6 +7,9 @@
 
 <main>
     <Container>
-        <Root />
+        <Router>
+            <Route path="/" component={Root} />
+            <Route path="/login" component={Login} />
+        </Router>
     </Container>
 </main>
