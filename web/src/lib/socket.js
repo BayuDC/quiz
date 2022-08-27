141793689b8b845ab775defeab2e7111915d89d1
BayuDC <dev.bayudc@gmail.com>
2022-08-27 22:23:31 +0700

    fix: one letter broke the ws

--- web/src/lib/socket.js
+++ web/src/lib/socket.js
@@ -1,6 +1,6 @@
 export default () =>
     new WebSocket(
         import.meta.env.PROD
-            ? document.URL.replace(/http/, 'ws') + '/api/auth/present'
+            ? document.URL.replace(/http/, 'ws') + 'api/auth/present'
             : 'ws://127.0.0.1:8080/api/auth/present'
     );
