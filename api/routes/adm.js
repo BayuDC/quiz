85ac842a07f1fc4274fb6756a4d722d2d35c4ee1
BayuDC <dev.bayudc@gmail.com>
2022-08-27 21:45:16 +0700

    feat: show stundets status

--- api/routes/adm.js
+++ api/routes/adm.js
@@ -8,16 +8,34 @@ module.exports = function (fastify, options, done) {
     fastify.get('/adm', {
         onRequest: fastify.basicAuth,
         handler: async (_, reply) => {
-            return reply.view('/views/dashboard.ejs', {
-                students: await prisma.user.findMany({
-                    include: {
-                        answers: {
-                            orderBy: { questionId: 'asc' },
-                        },
-                        score: true,
+            const students = await prisma.user.findMany({
+                include: {
+                    answers: {
+                        orderBy: { questionId: 'asc' },
                     },
-                }),
-                questions: await prisma.question.findMany(),
+                    score: true,
+                },
+            });
+
+            for (let student of students) {
+                const presence = await fastify.redis.get(`${student.username}:presence`);
+                const status = await fastify.redis.get(`${student.username}:status`);
+                if (presence) {
+                    if (status == 'started') {
+                        student.status = 'Working';
+                    } else {
+                        student.status = 'Online';
+                    }
+                } else {
+                    student.status = 'Offline';
+                }
+            }
+
+            const questions = await prisma.question.findMany();
+
+            return reply.view('/views/dashboard.ejs', {
+                students,
+                questions,
             });
         },
     });
