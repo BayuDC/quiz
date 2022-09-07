37b19b32ac902a2a737fdc0a1b6b177fb21106e1
BayuDC <dev.bayudc@gmail.com>
2022-09-07 14:18:37 +0700

    refactor: separata admin handler

---
+++ api/handlers/adm.js
@@ -0,0 +1,40 @@
+const prisma = require('../db');
+
+const getEverything = async that => {
+    const students = await prisma.user.findMany({
+        include: {
+            answers: {
+                orderBy: { questionId: 'asc' },
+            },
+            score: true,
+        },
+    });
+
+    for (let student of students) {
+        const presence = await that.redis.get(`${student.username}:presence`);
+        const status = await that.redis.get(`${student.username}:status`);
+        if (presence) {
+            if (status == 'started') {
+                student.status = 'Working';
+            } else {
+                student.status = 'Online';
+            }
+        } else {
+            student.status = 'Offline';
+        }
+    }
+
+    const questions = await prisma.question.findMany();
+
+    return {
+        students,
+        questions,
+    };
+};
+
+module.exports = {
+    async pretty(_, reply) {
+        const data = await getEverything(this);
+        return reply.view('views/dashboard.ejs', data);
+    },
+};
--- api/routes/adm.js
+++ api/routes/adm.js
@@ -1,4 +1,4 @@
-const prisma = require('../db');
+const handler = require('../handlers/adm');
 /**
  * @param {import('fastify').FastifyInstance} fastify
  * @param {import('fastify').FastifyPluginOptions} options
@@ -7,37 +7,7 @@ const prisma = require('../db');
 module.exports = function (fastify, options, done) {
     fastify.get('/adm', {
         onRequest: fastify.basicAuth,
-        handler: async (_, reply) => {
-            const students = await prisma.user.findMany({
-                include: {
-                    answers: {
-                        orderBy: { questionId: 'asc' },
-                    },
-                    score: true,
-                },
-            });
-
-            for (let student of students) {
-                const presence = await fastify.redis.get(`${student.username}:presence`);
-                const status = await fastify.redis.get(`${student.username}:status`);
-                if (presence) {
-                    if (status == 'started') {
-                        student.status = 'Working';
-                    } else {
-                        student.status = 'Online';
-                    }
-                } else {
-                    student.status = 'Offline';
-                }
-            }
-
-            const questions = await prisma.question.findMany();
-
-            return reply.view('/views/dashboard.ejs', {
-                students,
-                questions,
-            });
-        },
+        handler: handler.pretty,
     });
 
     done();
